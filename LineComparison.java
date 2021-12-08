d7299181bfe3ab794c6a2f00691615b18a4ec373
Shivangi Ranjan <shivangiec1098@gmail.com>
2021-12-08 20:18:57 +0530

    Compare and check which one is greater , lessor or equal

--- LineComparison.java
+++ LineComparison.java
@@ -18,30 +18,25 @@ public class LineComparison {
         double x22=sc.nextDouble();
         double y22=sc.nextDouble();
 
-        double lengthOfLine1=Length1(x11,x21,y11,y21);
+        double lengthOfLine1=Length(x11,x21,y11,y21);
         System.out.println("Length of line-1=" + lengthOfLine1);
-        double lengthOfLine2=Length2(x12,x22,y12,y22);
+        double lengthOfLine2=Length(x12,x22,y12,y22);
         System.out.println("Length of line 2= " + lengthOfLine2);
 
         if (Double.compare(lengthOfLine1, lengthOfLine2) == 0)
             System.out.println("Both lines are equal");
         else if (Double.compare(lengthOfLine1, lengthOfLine2) < 0)
-            System.out.println("line1 is greater then line 2");
-        else
             System.out.println("line 2 is greater then line 1");
+        else
+            System.out.println("line 1 is greater then line 2");
 
     }
-    public static double Length1(double x1, double x2, double y1, double y2)
+    public static double Length(double x1, double x2, double y1, double y2)
     {
         double len=Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
         return len;
     }
-    public static double Length2(double x1, double x2, double y1, double y2)
-    {
-        double len=Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-        return len;
-    }
-
 
 }
+
 
